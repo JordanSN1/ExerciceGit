6a1ad72e97fc7e6f7253893ceb2f01a15023491b
jordanSN1 <jordan.turnaco@ecoles-epsi.net>
2024-01-31 12:16:03 +0100

    correction d'une erreur , ajout de la boucle while afin que l'utilisateur puissent recommencer le programme ou non

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-NombreUtilisateur = int(input("Entrez un nombre: "))
-for i in range(0,11):
-    print(NombreUtilisateur * i)
+while True:
+    NombreUtilisateur = int(input("Entrez un nombre: "))
+
+    for i in range(1, 11):
+        print(f"{NombreUtilisateur} x {i} = {NombreUtilisateur * i}")
+
+    continuer = input("Voulez-vous recommencer le programme ? (Y/N): ").upper()
+    if continuer != 'Y':
+        break
